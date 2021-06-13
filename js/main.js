3c9279844c1e4dd2ff5a32ca02b3a1c922213c51
R0dionix <57255802+R0dionix@users.noreply.github.com>
2021-06-13 14:06:34 +0300

    обновление стилей и скриптов

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 
 // ------------------ Фиксированная шапка c расчётом высоты ------------------
 	let header = $("#header");
+    let headerInner = $(".header__inner");
     let main = $("#main");
     let mainHeight = main.innerHeight();
     let scrollPos = $(window).scrollTop();
@@ -18,8 +19,10 @@ $(document).ready(function(){
     function checkScroll(scrollPos, introH) {
         if( scrollPos > mainHeight ) {
             header.addClass("header--fixed");
+            headerInner.addClass("header__inner--fixed");
         } else {
             header.removeClass("header--fixed");
+            headerInner.removeClass("header__inner--fixed");
         }
     }
 

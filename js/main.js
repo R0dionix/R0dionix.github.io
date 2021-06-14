f6eca54d77d6776e6287c2818d429c1fe38a5254
R0dionix <57255802+R0dionix@users.noreply.github.com>
2021-06-14 17:56:58 +0300

    тестирование на выявление проблем

--- js/main.js
+++ js/main.js
@@ -1,30 +1,30 @@
 $(document).ready(function(){
 
 // ------------------ Фиксированная шапка c расчётом высоты ------------------
-	let header = $("#header");
-    let headerInner = $(".header__inner");
-    let main = $("#main");
-    let mainHeight = main.innerHeight();
-    let scrollPos = $(window).scrollTop();
+	// let header = $("#header");
+ //    let headerInner = $(".header__inner");
+ //    let main = $("#main");
+ //    let mainHeight = main.innerHeight();
+ //    let scrollPos = $(window).scrollTop();
 
-    checkScroll(scrollPos, mainHeight);
+ //    checkScroll(scrollPos, mainHeight);
 
-    $(window).on("scroll resize", function() {
-        mainHeight = main.innerHeight();
-        scrollPos = $(this).scrollTop();
+ //    $(window).on("scroll resize", function() {
+ //        mainHeight = main.innerHeight();
+ //        scrollPos = $(this).scrollTop();
 
-        checkScroll(scrollPos, mainHeight);
-    });
+ //        checkScroll(scrollPos, mainHeight);
+ //    });
 
-    function checkScroll(scrollPos, introH) {
-        if( scrollPos > mainHeight ) {
-            header.addClass("header--fixed");
-            headerInner.addClass("header__inner--fixed");
-        } else {
-            header.removeClass("header--fixed");
-            headerInner.removeClass("header__inner--fixed");
-        }
-    }
+ //    function checkScroll(scrollPos, introH) {
+ //        if( scrollPos > mainHeight ) {
+ //            header.addClass("header--fixed");
+ //            headerInner.addClass("header__inner--fixed");
+ //        } else {
+ //            header.removeClass("header--fixed");
+ //            headerInner.removeClass("header__inner--fixed");
+ //        }
+ //    }
 
 // ------------------ MixItUp ------------------
     mixitup('#portfolio__inner', {

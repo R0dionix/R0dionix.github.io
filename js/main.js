11d9bc2168ed0e047329abc74a5d0c66a84a953f
R0dionix <57255802+R0dionix@users.noreply.github.com>
2021-06-14 19:49:31 +0300

    проверка

--- js/main.js
+++ js/main.js
@@ -39,7 +39,7 @@ $(document).ready(function(){
     // Инициализация слайдера
       $('.feedback__slider').slick({
         infinite: true,
-        // autoplay: true,
+        autoplay: true,
         arrows: false,
         dots: true
       });
@@ -53,20 +53,20 @@ $(document).ready(function(){
 
 
     // // Плавная прокрутка
-    $("body").on('click', '[href*="#"]', function(e){
-        event.preventDefault();
+    // $("body").on('click', '[href*="#"]', function(e){
+    //     event.preventDefault();
 
-        $('.burger-btn, .header__menu').removeClass('active');
-        $('body').removeClass('overflow');
+    //     $('.burger-btn, .header__menu').removeClass('active');
+    //     $('body').removeClass('overflow');
 
-      $("html, body").animate({
-        scrollTop: $($(this).attr("href")).offset().top + "px"
-      }, {
-        duration: 1000,
-        easing: "swing"
-      });
-      return false;
-    });
+    //   $("html, body").animate({
+    //     scrollTop: $($(this).attr("href")).offset().top + "px"
+    //   }, {
+    //     duration: 1000,
+    //     easing: "swing"
+    //   });
+    //   return false;
+    // });
 
 
 });
